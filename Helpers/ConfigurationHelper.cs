1906a1b708cbac3a5934d90666ca92f1f94f90ce
Muhammad Umar <Muhammad Umar@DESKTOP-0VBRULH>
2024-05-27 11:34:30 +0500

    Add project files.

---
+++ Helpers/ConfigurationHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+public static class ConfigurationHelper
+{
+    private static IConfigurationRoot configuration;
+
+    static ConfigurationHelper()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        configuration = builder.Build();
+    }
+
+    public static string GetConnectionString(string name)
+    {
+        return configuration.GetConnectionString(name);
+    }
+
+    public static string GetAppSetting(string key)
+    {
+        return configuration[key];
+    }
+}
