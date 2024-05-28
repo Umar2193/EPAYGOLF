c3013f2bf72a27b517e69d1b8f8b739f2b0e7d19
Muhammad Umar <Muhammad Umar@DESKTOP-0VBRULH>
2024-05-28 14:18:52 +0500

    Template Customized

--- EPAYGOLF/Program.cs
+++ EPAYGOLF/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Data}/{action=Index}/{id?}");
 
 app.Run();
